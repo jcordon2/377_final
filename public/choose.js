9d899e4b5bd1f7f8b6daf20d21b7725383ca304c
Juliana Cordon <jcordon2@terpmail.umd.edu>
2026-05-17 14:22:08 -0400

    deftonesrn

--- public/choose.js
+++ public/choose.js
@@ -5,3 +5,13 @@ function startApp() {
 function goAbout () {
     window.location="quickbat.html";
 }
+
+function saveChoices() {
+    const mediaType = document.getElementById('mediaType').value;
+    const amount = document.getElementById('amount').value;
+
+    localStorage.setItem('mediaType', mediaType);
+    localStorage.setItem('amount', amount);
+
+    window.location = 'mewd.html';
+}
